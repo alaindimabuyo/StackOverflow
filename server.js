7d3d14867233b4714e6c3414f6b8445376436099
alain dimabuyo <xalaindimabuyo@gmail.com>
2020-05-22 01:11:01 +0800

    heroku configs

--- server.js
+++ server.js
@@ -13,13 +13,25 @@ app.use('/', require('./routes/questions'))
 const port = process.env.PORT || 8000;
 
 
-
+//Cors Headers | prevent cors errors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
+      return res.status(200).json({});
+    }
+    next();
+  });
+  
 //Handle Production
 if (process.env.NODE_ENV === "production") {
-    //set static folder
-    app.use(express.static(__dirname + "/public/"));
-    //handle spa
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
 
