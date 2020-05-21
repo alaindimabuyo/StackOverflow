1b32b7be5636dcfc303c802b36c8b557090efeaa
alain dimabuyo <xalaindimabuyo@gmail.com>
2020-05-22 00:58:06 +0800

    git status

--- server.js
+++ server.js
@@ -13,20 +13,7 @@ app.use('/', require('./routes/questions'))
 const port = process.env.PORT || 8000;
 
 
-//Cors Headers | prevent cors errors
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
-      return res.status(200).json({});
-    }
-    next();
-  });
-  
+
 //Handle Production
 if (process.env.NODE_ENV === "production") {
     //set static folder
